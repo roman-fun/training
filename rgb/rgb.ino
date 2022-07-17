bb38661a60402c2da52d9745009f349d5bbe0614
Alex Paramonov <aparamonov@datagrok.ai>
2022-07-17 11:49:35 +0300

    new task

---
+++ rgb/rgb.ino
@@ -0,0 +1,20 @@
+#include <Adafruit_NeoPixel.h>
+#define PIN  13
+#define NUMPIXELS 64
+Adafruit_NeoPixel strip (NUMPIXELS, 13, NEO_GRB + NEO_KHZ800);
+
+
+void setup() {
+  strip.begin();
+  strip.setBrightness(128);
+  strip.clear();
+  for (int i = 0; i < NUMPIXELS; i++) {
+    int c = map(i, 0, NUMPIXELS - 1, 0, 255);
+    strip.setPixelColor(i, strip.Color(255 - c, 0, c));
+  }
+  strip.show();
+}
+
+void loop() {
+
+}
